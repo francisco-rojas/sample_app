07f2707adcdc3cd457cd9c9bb56beff4739ed9ce
francisco-rojas <francisco.rojas@admios-sa.com>
2011-08-09 08:55:57 -0500

    added self keyword to sign_in function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
   
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    current_user = user
+    self.current_user = user
   end
   
   def current_user=(user)
@@ -20,6 +20,14 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)
     current_user = nil
+  end
+  
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def current_user?(user)
+    user == current_user
   end
 
     private
