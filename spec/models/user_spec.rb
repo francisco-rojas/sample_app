a0a881300ca71772f1483e6aae4d4f595295de9e
francisco-rojas <francisco.rojas@admios-sa.com>
2011-08-10 16:40:51 -0500

    added micropost model and associations with the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -152,6 +152,17 @@ describe User do
     end
   end
   
+  describe "micropost associations" do
+
+    before(:each) do
+      @user = User.create(@attr)
+    end
+
+    it "should have a microposts attribute" do
+      @user.should respond_to(:microposts)
+    end
+  end
+  
 end
 
 
