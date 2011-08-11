56d545dcc9f9b99f880de984ebd43883a36eb05f
francisco-rojas <francisco.rojas@admios-sa.com>
2011-08-11 15:44:07 -0500

    added new home page for signed in users

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,13 @@ class MicropostsController < ApplicationController
   before_filter :authenticate #, :only => [:create, :destroy]  |-> necesary only if another accion is added and the user does not need to be authenticated to access it
 
   def create
+    @micropost  = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_path
+    else
+      render 'pages/home'
+    end
   end
 
   def destroy
