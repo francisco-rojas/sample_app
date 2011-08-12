01d162c80b3463f202434607270b9ad9279e5133
francisco-rojas <francisco.rojas@admios-sa.com>
2011-08-12 16:53:30 -0500

    jquery code for characters counters in the submit micropost form is done

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,14 +3,15 @@ $(document).ready(function() {
 		var maxValue = 140;		
 		var charsEntered = $("#micropost_content").val().length;
 		var leftChars = maxValue - charsEntered;
-		if(leftChars <= 0)
+		if(leftChars < 0)
 		{			
-			$("submit_button").attr("disable", "true");
+			$("#submit_button").attr('disabled','disabled')
 			$("#chars_left").html("<label id=\"chars_left\" class=\"no_space_left\">" + leftChars + " characters left" + "</label>");			
 		}
 		else
 		{
 			$("#chars_left").html("<label id=\"chars_left\">" + leftChars + " characters left" + "</label>");
+			$("#submit_button").removeAttr('disabled');
 		}
 	}); 
 });
