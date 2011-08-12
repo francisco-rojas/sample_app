17b7e27642fb9b8d4cc9e0a5a60f212755f47f7a
francisco-rojas <francisco.rojas@admios-sa.com>
2011-08-12 16:33:33 -0500

    added characters counter in the submit micropost from

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$("#micropost_content").bind("keyup", function(){	
+		var maxValue = 140;		
+		var charsEntered = $("#micropost_content").val().length;
+		var leftChars = maxValue - charsEntered;
+		if(leftChars <= 0)
+		{			
+			$("submit_button").attr("disable", "true");
+			$("#chars_left").html("<label id=\"chars_left\" class=\"no_space_left\">" + leftChars + " characters left" + "</label>");			
+		}
+		else
+		{
+			$("#chars_left").html("<label id=\"chars_left\">" + leftChars + " characters left" + "</label>");
+		}
+	}); 
+});
