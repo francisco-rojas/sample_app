9b7666b004227707820466545759a033b610eac0
francisco-rojas <francisco.rojas@admios-sa.com>
2011-08-15 13:09:14 -0500

    added the followers relationship to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
   has_many :microposts, :dependent => :destroy
-  has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy  
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   has_many :following, :through => :relationships, :source => :followed
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
